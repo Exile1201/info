756fbee4852e16298998e14d0bbaa95bc23318de
Exile1201 <41160123+Exile1201@users.noreply.github.com>
2021-02-27 08:51:40 +0900

    Update aknav.js

--- js/aknav.js
+++ js/aknav.js
@@ -150,14 +150,14 @@ $(document).ready(function(){
                 
                 <li class="nav-item dropdown" id="navitemLanguage">
                     <a class="nav-link dropdown-toggle" style="display:inline-flex;padding-left:25px"href="#" id="languageDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                            <div class="ak-subtitle ak-disable" translate-id="language-2">Language</div>
+                            <div class="ak-subtitle ak-enable" translate-id="language-2">Language</div>
                             <div class="ak-disable" id="display-lang">`+langText+`</div>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item lang unselectable" onclick="langDropdown($(this))" value="en">English</a>
+                        <a class="dropdown-item lang selectable" onclick="langDropdown($(this))" value="en">English</a>
                         <a class="dropdown-item lang selectable" onclick="langDropdown($(this))" value="cn">Chinese</a>
-                        <a class="dropdown-item lang unselectable" onclick="langDropdown($(this))" value="jp">Japanese</a>
-                        <a class="dropdown-item lang unselectable" onclick="langDropdown($(this))" value="kr">Korean</a>
+                        <a class="dropdown-item lang selectable" onclick="langDropdown($(this))" value="jp">Japanese</a>
+                        <a class="dropdown-item lang selectable" onclick="langDropdown($(this))" value="kr">Korean</a>
                     </div>
                 </li>
             </ul>
