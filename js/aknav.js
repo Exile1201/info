01f68eca0bc77ceabb96873ac2c299de26a8e54c
Exile1201 <41160123+Exile1201@users.noreply.github.com>
2021-02-27 08:51:06 +0900

    Update aknav.js

--- js/aknav.js
+++ js/aknav.js
@@ -115,13 +115,11 @@ $(document).ready(function(){
 
     // console.log(localStorage.getItem('gameRegion'))
 
-    if(!localStorage.getItem('gameRegion') || !localStorage.getItem('webLang')){
-        localStorage.setItem("gameRegion", 'en');
+    if(!localStorage.getItem('webLang')){
         localStorage.setItem("webLang", 'en');
-        reg = "cn";
+     
         lang = "en";
     } else {
-        reg = localStorage.getItem('gameRegion');
         lang = localStorage.getItem('webLang');
     }
     switch (lang) {
@@ -149,19 +147,7 @@ $(document).ready(function(){
                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ace-donatemodal"><img height='20px' style='border:0px;height:20px;margin-top:-5px' src='./img/items/GOLD.png'>  Donate</button>
                 </li>
 
-                <li class="nav-item dropdown" id="navitemRegion">
-                        <a class="nav-link dropdown-toggle" style="display:inline-flex;padding-left:25px;"href="#" id="regionDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" >
-                                <div class="ak-subtitle ak-disable" translate-id="language-1">Server</div>
-                                <div class="ak-disable" id="display-reg">${reg.toUpperCase()}</div>
-                        </a>
-                        
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item reg unselectable" onclick="regDropdown($(this))" value="cn">Chinese</a>
-                            <a class="dropdown-item reg selectable" onclick="regDropdown($(this))" value="en">English</a>
-                            <a class="dropdown-item reg unselectable" onclick="regDropdown($(this))" value="jp">Japanese</a>
-                            <a class="dropdown-item reg unselectable" onclick="regDropdown($(this))" value="kr">Korean</a>
-                        </div>
-                    </li>
+                
                 <li class="nav-item dropdown" id="navitemLanguage">
                     <a class="nav-link dropdown-toggle" style="display:inline-flex;padding-left:25px"href="#" id="languageDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                             <div class="ak-subtitle ak-disable" translate-id="language-2">Language</div>
@@ -169,7 +155,7 @@ $(document).ready(function(){
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item lang unselectable" onclick="langDropdown($(this))" value="en">English</a>
-                        <a class="dropdown-item lang unselectable" onclick="langDropdown($(this))" value="cn">Chinese</a>
+                        <a class="dropdown-item lang selectable" onclick="langDropdown($(this))" value="cn">Chinese</a>
                         <a class="dropdown-item lang unselectable" onclick="langDropdown($(this))" value="jp">Japanese</a>
                         <a class="dropdown-item lang unselectable" onclick="langDropdown($(this))" value="kr">Korean</a>
                     </div>
