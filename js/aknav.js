b5b1d6b806ef6e0f43eb2e95c6d63319be2c2f77
Exile1201 <41160123+Exile1201@users.noreply.github.com>
2021-02-27 10:17:21 +0900

    Update aknav.js

--- js/aknav.js
+++ js/aknav.js
@@ -110,4 +110,11 @@ $(document).ready(function(){
             navDropdown.push(`</div>`)
         }
     })
+    let html = `
+    <img src="./img/custom/texas.jpg" width="40" height="40" style="transform:scale(1.2,1.2)translate(-8px,1px)"class="d-inline-block align-top" alt="">
+        <a class="navbar-brand" href="index.html" translate-id="topbar-1">Exile1201</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="true" aria-label="展开">
+            <span class="navbar-toggler-icon"></span>
+        </button>
     
