d357226b48dfadafe6bb8278af7d4adbabc696d2
Exile1201 <41160123+Exile1201@users.noreply.github.com>
2021-02-27 10:00:41 +0900

    Update aknav.js

--- js/aknav.js
+++ js/aknav.js
@@ -132,8 +132,8 @@ $(document).ready(function(){
     }
     // console.log(reg)
     let html = `
-    <img src="./img/factions/logo_rhodes.png" width="40" height="40" style="transform:scale(1.2,1.2)translate(-8px,1px)"class="d-inline-block align-top" alt="">
-        <a class="navbar-brand" href="index.html" translate-id="topbar-1">Arknights Toolbox</a>
+    <img src="./img/custom/texas.jpg" width="40" height="40" style="transform:scale(1.2,1.2)translate(-8px,1px)"class="d-inline-block align-top" alt="">
+        <a class="navbar-brand" href="index.html" translate-id="topbar-1">Exile1201</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="展开">
             <span class="navbar-toggler-icon"></span>
