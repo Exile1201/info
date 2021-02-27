9763d02a210a9a46fe7b7a212bfe2b28c3da5ef2
Exile1201 <41160123+Exile1201@users.noreply.github.com>
2021-02-27 09:38:02 +0900

    Update aknav.js

--- js/aknav.js
+++ js/aknav.js
@@ -91,13 +91,13 @@ $(document).ready(function(){
             navDropdown.push(`
             <li class="nav-item dropdown ${isCurrent!="menu"&&isGroup?"active":""}">
             <a class="nav-link dropdown-toggle" style="display:inline-flex;padding-left:25px;"href="#" id="regionDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" >
-                    <div class="ak-subtitle ak-enable">${drop.name}</div>
+                    <div class="ak-subtitle ak-disable">${drop.name}</div>
             `)
             
             
             // console.log(isCurrent)
             navDropdown.push(`
-                    <div class="ak-enable ">${isCurrent}</div>
+                    <div class="ak-disable ">${isCurrent}</div>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             `)
@@ -110,15 +110,18 @@ $(document).ready(function(){
             navDropdown.push(`</div>`)
         }
     })
+    var reg;
     var lang, langText;
 
     // console.log(localStorage.getItem('gameRegion'))
 
-    if(!localStorage.getItem('webLang')){
+    if(!localStorage.getItem('gameRegion') || !localStorage.getItem('webLang')){
+        localStorage.setItem("gameRegion", 'cn');
         localStorage.setItem("webLang", 'en');
-     
+        reg = "cn";
         lang = "en";
     } else {
+        reg = localStorage.getItem('gameRegion');
         lang = localStorage.getItem('webLang');
     }
     switch (lang) {
@@ -129,10 +132,10 @@ $(document).ready(function(){
     }
     // console.log(reg)
     let html = `
-    <img src="./img/custom/texas.jpg" width="40" height="40" style="transform:scale(1.2,1.2)translate(-8px,1px)"class="d-inline-block align-top" alt="">
-        <a class="navbar-brand" href="index.html" translate-id="topbar-1">Exile1201</a>
+    <img src="./img/factions/logo_rhodes.png" width="40" height="40" style="transform:scale(1.2,1.2)translate(-8px,1px)"class="d-inline-block align-top" alt="">
+        <a class="navbar-brand" href="index.html" translate-id="topbar-1">Arknights Toolbox</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="true" aria-label="展开">
+                aria-controls="navbarNav" aria-expanded="false" aria-label="展开">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
@@ -146,7 +149,19 @@ $(document).ready(function(){
                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ace-donatemodal"><img height='20px' style='border:0px;height:20px;margin-top:-5px' src='./img/items/GOLD.png'>  Donate</button>
                 </li>
 
-                
+                <li class="nav-item dropdown" id="navitemRegion">
+                        <a class="nav-link dropdown-toggle" style="display:inline-flex;padding-left:25px;"href="#" id="regionDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" >
+                                <div class="ak-subtitle ak-disable" translate-id="language-1">Server</div>
+                                <div class="ak-disable" id="display-reg">${reg.toUpperCase()}</div>
+                        </a>
+                        
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item reg unselectable" onclick="regDropdown($(this))" value="cn">Chinese</a>
+                            <a class="dropdown-item reg unselectable" onclick="regDropdown($(this))" value="en">English</a>
+                            <a class="dropdown-item reg unselectable" onclick="regDropdown($(this))" value="jp">Japanese</a>
+                            <a class="dropdown-item reg unselectable" onclick="regDropdown($(this))" value="kr">Korean</a>
+                        </div>
+                    </li>
                 <li class="nav-item dropdown" id="navitemLanguage">
                     <a class="nav-link dropdown-toggle" style="display:inline-flex;padding-left:25px"href="#" id="languageDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                             <div class="ak-subtitle ak-disable" translate-id="language-2">Language</div>
@@ -179,13 +194,13 @@ $(document).ready(function(){
             <div class="modal-body acedonate-body">
             	<div style="text-align:left; margin: 5px;">
             		<h5>Help us maintain the website</h5>
-            		<p>#</p>
-            		<p>#</p>
-            		<p>#</p>
+            		<p>We made this website on our spare time and it took us quite some time to get into what it is now. Currently we do have some list of new features that we want to implement, but the progress is slow because we only work on it when we have the time and motivation to do it.</p>
+            		<p>Aside from implementing a new feature, fixing bugs, and receiving feedbacks, we prioritizes updating our database whenever the game got updated.
+            		<p>Your donation will definitely help on improving the website, keeping it updated, and motivating us to keep doing what we're doing and this is completely optional for you to do.</p>
             	</div>
             	<div class="row" style="padding: 10px; max-height: 160px; background-color: #333;">
             		<div class="col-md-6">
-	            		<a class="btn btn-info" style="padding:15px; width: 100%; height: 100%" href='#' target='_blank'>
+	            		<a class="btn btn-info" style="padding:15px; width: 100%; height: 100%" href='https://ko-fi.com/aceship' target='_blank'>
 	            			<b style=" color: white;">Donate on Ko-fi</b>
 	            			<div style="display:block; margin-top: 20px;">
 	            				<img height="50" src="https://theme.zdassets.com/theme_assets/2141020/171bb773b32c4a72bcc2edfee4d01cbc00d8a004.png">
@@ -194,7 +209,7 @@ $(document).ready(function(){
 	            		</a>
             		</div>
             		<div class="col-md-6">
-	            		<a class="btn btn-info" style="padding:15px 10px 10px 10px; width: 100%;" href='#' target='_blank'>
+	            		<a class="btn btn-info" style="padding:15px 10px 10px 10px; width: 100%;" href='https://trakteer.id/Aceship123' target='_blank'>
 	            			<b style=" color: white;">Donate on Trakteer</b>
 	            			<div style="display:block;">
 	            				<div style="background-color: #bf202f; display: inline-block; border-radius: 6px; padding: 5px 10px; vertical-align: middle;">
