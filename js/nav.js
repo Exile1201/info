45547349b80e575f0047d24c8c539fe1e4b45b23
Exile1201 <41160123+Exile1201@users.noreply.github.com>
2021-03-06 23:47:01 +0900

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
         name:"Projects",
         content:[
         {
-            href:"pixelgame.html",
+            href:"project/pixelgame.html",
             name:"Pixel Game"
         },{
             href:"#",
